26b4b0d8174ce3ef3bcf38a2f537f92e96fdea06
Lubomir Plevjak <lplevjakk@gmail.com>
2026-01-29 09:06:22 +0100

    Update auto.py

--- auto.py
+++ auto.py
@@ -10,25 +10,53 @@ import getpass
 # Príklad: users = [("meno", "heslo"), ("meno2", "heslo2")]
 users = []
 
+# Počet lekcií ktoré sa majú urobiť naraz v jednom balíku
+POCET_LEKCII = 3
+
 def vsetky_baliky(woca):
     while woca.get_packages(woca.DOPACKAGE):
         woca.pick_package(0,woca.get_packages(woca.DOPACKAGE))
-        while True:
+        
+        # Urob POCET_LEKCII lekcií naraz
+        for lekcia in range(POCET_LEKCII):
+            print(f"[*] Robím lekciu {lekcia + 1}/{POCET_LEKCII}")
+            while True:
+                try:
+                    woca.do_package()
+                except Exception as e:
+                    print(traceback.format_exception(e))
+                else:
+                    break
+            sleep(2)
+            
+            # Po každej lekcii klikni na Continue ak existuje
+            if woca.exists_element(woca.driver, By.ID, "continueBtn"):
+                woca.get_element(By.ID, "continueBtn").click()
+                sleep(1)
+            
+            # Skontroluj či ešte môžeme pokračovať v ďalšej lekcii
             try:
-                woca.do_package()
-            except Exception as e:
-                print(traceback.format_exception(e))
-            else:
-                break
+                woca.wait_for_element(3, By.ID, "backBtn")
+                back_text = woca.get_element_text(By.ID, "backBtn")
+                if back_text == "Uložiť a odísť":
+                    # Balík dokončený, ukončíme loop
+                    print(f"[*] Balík dokončený po {lekcia + 1} lekciách")
+                    woca.get_element(By.ID, "backBtn").click()
+                    break
+            except:
+                # Ak nie je backBtn, skúsime pokračovať
+                pass
+        
         sleep(2)
-        if woca.exists_element(woca.driver,By.ID,"continueBtn"):
-            woca.get_element(By.ID,"continueBtn").click()
+        # Finálna kontrola po všetkých lekciách
+        if woca.exists_element(woca.driver, By.ID, "continueBtn"):
+            woca.get_element(By.ID, "continueBtn").click()
         try:
-            woca.wait_for_element(5,By.ID,"backBtn")
-            if woca.get_element_text(By.ID,"backBtn") == "Uložiť a odísť":
-                woca.get_element(By.ID,"backBtn").click()
+            woca.wait_for_element(5, By.ID, "backBtn")
+            if woca.get_element_text(By.ID, "backBtn") == "Uložiť a odísť":
+                woca.get_element(By.ID, "backBtn").click()
         except:
-            exit(0)
+            pass
 
 def do_wocabee(user):
     woca = wocabee.wocabee(user)
